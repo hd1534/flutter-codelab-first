4e77339ab04202fba3c9220dd232c2ab003e2d7f
nahjh2 <nahjh2@ajou.ac.kr>
2025-12-02 07:22:27 +0000

    add future builder

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,8 @@ class MyHttpOverrides extends HttpOverrides {
 void main() {
   // https://nahjh2.web.ajousw.kr는 ssl이 제대로 설정되어 있지 않으므로.
   HttpOverrides.global = MyHttpOverrides();
-  runApp(MyApp());
+  runApp(ChangeNotifierProvider(
+      create: (context) => MyAppState(), child: const MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -34,16 +35,27 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => MyAppState(),
-      child: MaterialApp(
-        title: 'Namer App',
-        theme: ThemeData(
-          useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
-        ),
-        home: MyHomePage(),
+    var appState = context.watch<MyAppState>();
+
+    return MaterialApp(
+      title: 'Namer App',
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
       ),
+      home: FutureBuilder<List<WordPair>>(
+          future: appState.futureFavorites,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(child: CircularProgressIndicator());
+            } else if (snapshot.hasError) {
+              return Center(child: Text('Error: ${snapshot.error}'));
+            } else if (snapshot.hasData) {
+              return MyHomePage();
+            } else {
+              return const Center(child: Text('No favorites yet.'));
+            }
+          }),
     );
   }
 }
